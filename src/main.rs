b87258ce29354f947ef7aa3db8155f3decfe1a04
Peter Navarrete <navarrep1@newpaltz.edu>
2023-05-15 18:57:47 -0400

    feat: gravity and floor collision work

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,22 @@ impl Ball {
     }
 
     pub fn update(&mut self, dt: f32) {
-        self.circ.x += self.vel.x * dt ;
+        self.circ.x += self.vel.x * dt;
         self.circ.y += self.vel.y * dt * GRAVITY;
+        self.vel.y += GRAVITY * dt;
+
+        if self.circ.x < 0f32 {
+            self.vel.x = self.vel.x * -1f32;
+        }
+        if self.circ.y > screen_height() {
+            self.vel.y = self.vel.y * -1f32;
+        }
+    }
+    pub fn draw(&self) {
+        draw_circle(self.circ.x, self.circ.y, self.circ.r, BLUE);
+    }
+    pub fn resolve_self_collision(&mut self,ball:&mut Ball) {
+        
     }
 
 }
@@ -32,6 +46,7 @@ async fn main() {
     let mut balls:Vec<Ball> = Vec::new();
 
     loop {
+        println!("time; {}", get_frame_time());
         if is_key_pressed(KeyCode::Space) {
             balls.push(Ball::new());
         }
@@ -40,6 +55,13 @@ async fn main() {
             ball.update(get_frame_time());
 
         }
-       
+        for ball in balls.iter_mut() {
+
+        }
+        clear_background(WHITE);
+        for ball in balls.iter() {
+            ball.draw();
+        } 
+        next_frame().await;
     }
 }
